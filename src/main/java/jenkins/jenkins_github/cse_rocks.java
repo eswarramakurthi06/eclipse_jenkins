939e7aa348883c52f76ceb5e1e778b455dfbf3f5
23mh1a05k4 <23mh1a05k4@acoe.edu.in>
2026-02-14 16:02:30 +0530

    syntax fix

--- src/main/java/jenkins/jenkins_github/cse_rocks.java
+++ src/main/java/jenkins/jenkins_github/cse_rocks.java
@@ -1,5 +1,10 @@
 package jenkins.jenkins_github;
 
 public class cse_rocks {
-
+	public int add(int a,int b) {
+		return a+b;
+	}
+	public int mul(int a,int b) {
+		return a*b;
+	}
 }
--- src/test/java/jenkins/jenkins_github/cse_rocksTest.java
+++ src/test/java/jenkins/jenkins_github/cse_rocksTest.java
@@ -1,14 +1,37 @@
 package jenkins.jenkins_github;
-
 import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.Test;
+//
+//class cse_rocks {
+//	cse_rocks cal=new cse_rocks();
+//	
+//	@Test
+//	void test() {
+//		int result = cal.add(2, 3);
+//		assertEquals(5,result);
+//	}
+//	
+//
+//	
+//	
+//
+//}
 
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class cse_rocksTest {
+class CseRocksTest {
+	cse_rocks cal = new cse_rocks();
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test1() {
+		int result = cal.mul(2, 3);
+		assertEquals(6, result);
 	}
-
 }
